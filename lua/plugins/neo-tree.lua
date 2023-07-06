4b6c98f8616c1a3f6e147f0127719583edada34b
Last <last7617@outlook.com>
2023-07-07 01:28:09 +0800

    modified neo-tree
    add mappings on switching between filesystem, buffers and git_status
    add config to make cursor invisible in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,7 +4,36 @@ local config = {
 
     window = {
         width = 30,
+
+        mappings = {
+            -- switch between filesystem, buffers and git_status
+            ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
+            ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
+            ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+        }
     },
+
+    -- hide cursor in neo-tree window
+    event_handlers = {
+        {
+            event = "neo_tree_buffer_enter",
+            handler = function ()
+                local hl = vim.api.nvim_get_hl_by_name('Cursor', true)
+                hl.blend = 100
+                vim.api.nvim_set_hl(0, 'Cursor', hl)
+                vim.opt.guicursor:append('a:Cursor/lCursor')
+            end
+        },
+        {
+            event = "neo_tree_buffer_leave",
+            handler = function ()
+                local hl = vim.api.nvim_get_hl_by_name('Cursor', true)
+                hl.blend = 0
+                vim.api.nvim_set_hl(0, 'Cursor', hl)
+                vim.opt.guicursor:append('a:Cursor/lCursor')
+            end
+        }
+    }
 }
 
 return {
